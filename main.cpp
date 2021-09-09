3d24e31c1811c8a27d13f6ed8c583bf573aa35e1
StalkerShurik <AlexanderGaponov@outlook.com>
2021-09-09 20:53:23 +0300

    First Commit

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::string changes="changes";
     std::cout << changes;
+    std::cout << changes << " " << changes;
+
     return 0;
 }
